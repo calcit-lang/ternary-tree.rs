8b1deee4baeea581d0813329b4e578129b2680e0
tiye <jiyinyiyong@gmail.com>
2024-02-08 16:21:52 +0800

    tag 0.0.14

--- tests/list_tests.rs
+++ tests/list_tests.rs
@@ -283,7 +283,7 @@ fn force_balancing() -> Result<(), String> {
   // echo data.formatInline
   assert_eq!(
     data.format_inline(),
-    String::from("((((0 1 2) 3 (4 5 6)) 7 ((8 9 10) 11 (12 13 14))) 15 ((16 17 18) 19))")
+    String::from("((0 1 2) (((3 4 5) (6 7 8) (9 10 11)) ((12 13 14) (15 16 17))) (18 19))") // deeper in middle
   );
   if let Err(msg) = data.force_inplace_balancing() {
     println!("[warning] {}", msg)
